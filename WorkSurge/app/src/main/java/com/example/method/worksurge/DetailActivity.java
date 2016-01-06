75733c204c869f25caf83a9017b855ce0a420af1
ninorachota <nino.rachota@gmail.com>
2016-01-06 15:27:53 +0100

    Detail and dutch language
    Detail activity, dutch language

---
+++ WorkSurge/app/src/main/java/com/example/method/worksurge/DetailActivity.java
@@ -0,0 +1,17 @@
+package com.example.method.worksurge;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nino on 4-1-2016.
+ */
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+    }
+}
