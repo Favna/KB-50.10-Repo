65d66480e6378f3884c95adab4c146be986a39ac
ninorachota <nino.rachota@gmail.com>
2016-01-18 11:46:52 +0100

    View preference
    Made the save view preference.

--- WorkSurge/app/src/main/java/com/example/method/worksurge/View/SaveViewPreferenceActivity.java
+++ WorkSurge/app/src/main/java/com/example/method/worksurge/View/SaveViewPreferenceActivity.java
@@ -1,20 +1,24 @@
 package com.example.method.worksurge.View;
 
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.example.method.worksurge.R;
 
 import java.io.OutputStreamWriter;
 
-public class SaveEmailActivity extends AppCompatActivity {
+/**
+ * Created by Gebruiker on 18-1-2016.
+ */
+public class SaveViewPreferenceActivity extends AppCompatActivity {
 
-    private final static String STORETEXT = "storeEmailText.txt";
-    private EditText txtEditor;
+    private final static String STORETEXT = "storeViewText.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +26,19 @@ public class SaveEmailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        txtEditor = (EditText)findViewById(R.id.TextEmail);
     }
 
-    public void saveEmail(View v){
+    public void saveViewPreference(View v){
+        RadioGroup rg = (RadioGroup)findViewById(R.id.radiobtngroupViewPreference);
+        int selected = rg.getCheckedRadioButtonId();
+        RadioButton rb = (RadioButton)findViewById(selected);
         try{
             OutputStreamWriter out = new OutputStreamWriter(openFileOutput(STORETEXT, 0));
-            out.write(txtEditor.getText().toString());
-            Toast.makeText(this, "Email is saved", Toast.LENGTH_SHORT).show();
+            out.write(rb.getText().toString());
+            Toast.makeText(this, "View preference is saved", Toast.LENGTH_SHORT).show();
         }catch(Throwable t){
             Toast.makeText(this, "Error"+t.toString(), Toast.LENGTH_LONG).show();
         }
     }
+
 }
