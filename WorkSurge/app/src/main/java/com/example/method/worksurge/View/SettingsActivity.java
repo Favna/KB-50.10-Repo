6cd93df541b08e1b45d030bf022d65b1df284e18
ninorachota <nino.rachota@gmail.com>
2016-01-18 15:14:01 +0100

    All moved
    Everything is moved to the settings

--- WorkSurge/app/src/main/java/com/example/method/worksurge/View/SettingsActivity.java
+++ WorkSurge/app/src/main/java/com/example/method/worksurge/View/SettingsActivity.java
@@ -6,10 +6,20 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.example.method.worksurge.R;
 
+import java.io.OutputStreamWriter;
+
 public class SettingsActivity extends AppCompatActivity {
+
+    private final static String STOREVIEWTEXT = "storeViewText.txt";
+    private final static String STOREEMAILTEXT = "storeEmailText.txt";
+    private EditText txtEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +27,30 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        txtEditor = (EditText)findViewById(R.id.TextEmail);
+    }
+
+    public void saveEmail(View v){
+        try{
+            OutputStreamWriter out = new OutputStreamWriter(openFileOutput(STOREEMAILTEXT, 0));
+            out.write(txtEditor.getText().toString());
+            Toast.makeText(this, "Email is saved", Toast.LENGTH_SHORT).show();
+        }catch(Throwable t){
+            Toast.makeText(this, "Error"+t.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void saveViewPreference(View v){
+        RadioGroup rg = (RadioGroup)findViewById(R.id.radiobtngroupViewPreference);
+        int selected = rg.getCheckedRadioButtonId();
+        RadioButton rb = (RadioButton)findViewById(selected);
+        try{
+            OutputStreamWriter out = new OutputStreamWriter(openFileOutput(STOREVIEWTEXT, 0));
+            out.write(rb.getText().toString());
+            Toast.makeText(this, "View preference is saved", Toast.LENGTH_SHORT).show();
+        }catch(Throwable t){
+            Toast.makeText(this, "Error"+t.toString(), Toast.LENGTH_LONG).show();
+        }
     }
 
 }
