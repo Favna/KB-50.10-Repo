350e7b69902c7509a01a0bc4304435e33c6bc274
MrRichTee <ostmyst@gmail.com>
2016-01-20 02:29:17 +0100

    Core/Map: Map added.

--- WorkSurge/app/src/main/java/com/example/method/worksurge/View/FoundVacanciesActivity.java
+++ WorkSurge/app/src/main/java/com/example/method/worksurge/View/FoundVacanciesActivity.java
@@ -1,5 +1,6 @@
 package com.example.method.worksurge.View;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,7 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.example.method.worksurge.Enum.FragmentEnum;
 import com.example.method.worksurge.Enum.IntentEnum;
--- WorkSurge/app/src/main/java/com/example/method/worksurge/View/MapFragment.java
+++ WorkSurge/app/src/main/java/com/example/method/worksurge/View/MapFragment.java
@@ -1,6 +1,7 @@
 package com.example.method.worksurge.View;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,9 +9,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.method.worksurge.Enum.IntentEnum;
 import com.example.method.worksurge.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    private View view;
+    private GoogleMap mMap;
 
     public MapFragment() {
         // Required empty public constructor
@@ -25,6 +35,17 @@ public class MapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        this.view = inflater.inflate(R.layout.fragment_map, container, false);
+        return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
