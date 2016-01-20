35402c2b92131a5cc0bb116da2cfacd704713759
MrRichTee <ostmyst@gmail.com>
2016-01-20 03:44:38 +0100

    Core/Map: Enable default options

--- WorkSurge/app/src/main/java/com/example/method/worksurge/View/MapFragment.java
+++ WorkSurge/app/src/main/java/com/example/method/worksurge/View/MapFragment.java
@@ -2,33 +2,29 @@ package com.example.method.worksurge.View;
 
 import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-import com.example.method.worksurge.Enum.IntentEnum;
 import com.example.method.worksurge.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     private View view;
-    private GoogleMap mMap;
 
     public MapFragment() {
         // Required empty public constructor
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
     }
 
     @Override
@@ -36,16 +32,29 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         this.view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.workSurgeGoogleMap);
+
+        if(mapFragment != null)
+            mapFragment.getMapAsync(this);
+        else
+            Toast.makeText(getContext(), "Map was not properly initialized", Toast.LENGTH_SHORT).show();
+
         return view;
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        LatLng sydney = new LatLng(-33.867, 151.206);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        googleMap.setMyLocationEnabled(true);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        googleMap.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
     }
+
 }
